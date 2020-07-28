37a44511574dd2517b8f0418051c3c532b103690
IWANABETHATGUY <974153916@qq.com>
2020-07-28 14:35:21 +0800

    feat: 🎸 level print

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::path::PathBuf;
-use tree_sitter::{Language, Parser};
+use tree_sitter::{Language, Parser, TreeCursor, Node};
 
 extern "C" {
     fn tree_sitter_vue() -> Language;
@@ -17,5 +17,17 @@ fn main() {
 "#;
 
     let tree = parser.parse(source_code, None).unwrap();
-    println!("{:?}", tree.root_node());
+    let node = tree.root_node();
+    pretty_print(node, 0);
+}
+
+fn pretty_print(root: Node, level: usize ) {
+    let kind = root.kind();
+    let start = root.start_position();
+    let end = root.end_position();
+    println!("{}{} [{}, {}] - [{}, {}]"," ".repeat(level * 2), kind, start.row, start.column, end.row, end.column);
+    for i in 0..root.named_child_count() {
+        let node = root.named_child(i).unwrap();
+        pretty_print(node, level + 1);
+    }
 }
