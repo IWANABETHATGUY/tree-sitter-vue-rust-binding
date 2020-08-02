a428f0bb3f16ce865da5ae1f50f1ef5694430714
IWANABETHATGUY <974153916@qq.com>
2020-08-02 11:29:49 +0800

    feat: 🎸 finish-rustbinding

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::path::PathBuf;
-use tree_sitter::{Language, Parser, TreeCursor, Node};
+use std::{fmt::Debug, path::PathBuf};
+use tree_sitter::{Language, Node, Parser, TreeCursor};
+use tree_sitter_vue_rust_binding::tree_sitter_vue;
 
-extern "C" {
-    fn tree_sitter_vue() -> Language;
-}
 fn main() {
     let language = unsafe { tree_sitter_vue() };
     let mut parser = Parser::new();
@@ -11,23 +9,35 @@ fn main() {
     let source_code = r#"
 <template>
   <p>
-    Hello, <a :[key]="url">{{ name }}</a>!
+    Hello, <a :[key]="url">{{ name 我的}}</a>!
   </p>
+//  j 
 </template>
 "#;
 
     let tree = parser.parse(source_code, None).unwrap();
     let node = tree.root_node();
-    pretty_print(node, 0);
+    pretty_print(&source_code, node, 0);
 }
 
-fn pretty_print(root: Node, level: usize ) {
+fn pretty_print(source_code: &str, root: Node, level: usize) {
+    if !root.is_named() {
+        println!("{:?}", &source_code[root.start_byte()..root.end_byte()]);
+    }
     let kind = root.kind();
     let start = root.start_position();
     let end = root.end_position();
-    println!("{}{} [{}, {}] - [{}, {}]"," ".repeat(level * 2), kind, start.row, start.column, end.row, end.column);
-    for i in 0..root.named_child_count() {
-        let node = root.named_child(i).unwrap();
-        pretty_print(node, level + 1);
+    println!(
+        "{}{} [{}, {}] - [{}, {}] ",
+        " ".repeat(level * 2),
+        kind,
+        start.row,
+        start.column,
+        end.row,
+        end.column
+    );
+    for i in 0..root.child_count() {
+        let node = root.child(i).unwrap();
+        pretty_print(source_code, node, level + 1);
     }
 }
